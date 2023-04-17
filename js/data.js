40a9d2b8e6abee6e8a6930308c0511f779dbfd73
Alexmajak <alexmajak@mail.ru>
2023-04-17 18:50:20 +0500

    new 17/04/2023

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const data = {
+	"first_name": "Alex",
+	"last_name": "Nikolaevich",
+	"job_title": "Engineer",
+	"photo": "images/w1.jpg",
+	"phone": "+8(922)47-47-29",
+	"email": "alexmajak@mail.ru",
+	"skype": "skype_number",
+	"linkedin": "link_to_linkedin",
+	"address": "vostok 3, kv27, Karaganda",
+	"skills": [
+		"Creative Development",
+		"Branding & Identity",
+		"Project Management",
+		"Contact Negotiation",
+		"Ad Buying & Planning",
+		"Product Positioning",
+		"Visual Merchandising",
+		"Search Engine Ads",
+		"Process Improvement",
+		"Market Strategizing",
+		"Adobe Creative Suite",
+		"Microsoft Office 2016",
+		"Google Web Analytics",
+		"Fluent in Spanish",
+	],
+	"education": [
+		["1999-2003", "University of Karaganda Technical University"],
+
+	],
+	"achievements": [
+		"Marketer of the Year Academic Scholorship Deans List: 2006-2007, National Merit Scholarship Corporation",
+	],
+	"profile": "This paragraph will be you first impressions to recruiters. An effective summary is short, concise and focuses on your most important selling points. You should align your summary with the job requirements by highlighting the key strengths and skills that will truly make you stand out as a highly qualified candidate. Transforming responsibilities into quantifiable achievements with numbers communicates your value."
+};
